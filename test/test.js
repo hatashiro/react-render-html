dbee3072835ad12aa4b3cd2a67b989e46caf3029
Spencer Elliott <me@elliottsj.com>
2016-06-05 15:27:12 -0400

    Upgrade ava to version ^0.15.2

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ import * as ReactDOMServer from 'react-dom/server';
 import renderHTML from '../index';
 
 let renderTest = (t, reactEl, expectedHTML) => {
-  t.same(ReactDOMServer.renderToStaticMarkup(reactEl), expectedHTML);
+  t.is(ReactDOMServer.renderToStaticMarkup(reactEl), expectedHTML);
 };
 
 let singeElementTest = (t, html) => {
@@ -22,7 +22,7 @@ test('returns a single React element rendering a provided HTML', t => {
 test('returns an array of React elements if several nodes are provided', t => {
   let arr = renderHTML('<li><a class="hello" href="https://github.com">hihi</a></li>' +
                        '<li><p><b>hello</b>world</p><p>react</p></li>');
-  t.same(arr.length, 2);
+  t.is(arr.length, 2);
   renderTest(t, arr[0], '<li><a class="hello" href="https://github.com">hihi</a></li>');
   renderTest(t, arr[1], '<li><p><b>hello</b>world</p><p>react</p></li>');
 });
